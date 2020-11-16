f6d54ac43e20c56a8ace508f90fb8ff7c1cab971
xtophe xanon <xocon10@gmail.com>
2020-11-16 09:35:40 -0600

    homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -8,9 +9,9 @@ const App = () => {
 		<React.Fragment>
 			<Header />
 			<main className='py-3'>
-        <Container>
-				<h1>proshop</h1>
-        </Container>
+				<Container>
+					<HomeScreen />
+				</Container>
 			</main>
 			<Footer />
 		</React.Fragment>
