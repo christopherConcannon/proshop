4c09999d2a7d3e6ea510982549d8bebdeebe8fb6
xtophe xanon <xocon10@gmail.com>
2020-11-17 19:41:29 -0600

    add qty select to ProductScreen.  set up add to cart handler and component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -14,6 +15,8 @@ const App = () => {
 				<Container>
 					<Route exact path='/' component={HomeScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
+          {/* optional id */}
+					<Route path='/cart/:id?' component={CartScreen} />
 				</Container>
 			</main>
 			<Footer />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,25 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProductDetails, listProducts } from '../actions/productActions'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 
-// https://reactrouter.com/core/api/match -- React Router has match object with params prop (also see useParams())
-const ProductScreen = ({ match }) => {
-	const productDetails = useSelector((state) => state.productDetails)
-	const { product, loading, error } = productDetails
+// https://reactrouter.com/core/api/match -- React Router has match object with params prop (also see useParams()).  also has a history prop that allows you to push https://reactrouter.com/core/api/history
+const ProductScreen = ({ history, match }) => {
+	const [ qty, setQty ] = useState(0)
 
 	const dispatch = useDispatch()
+
+	const productDetails = useSelector((state) => state.productDetails)
+	const { product, loading, error } = productDetails
 
 	useEffect(
 		() => {
 			dispatch(listProductDetails(match.params.id))
 		},
 		[ match, dispatch ]
-	)
+  )
+  // go to cart page with some parameters (product id and qty as query string -- http://localhost:3000/cart/5fb30918449c928f9cb54b18?qty=2)
+  const addToCartHandler = () => {
+    // props.history.push will redirect
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
 
 	return (
 		<React.Fragment>
@@ -65,11 +72,37 @@ const ProductScreen = ({ match }) => {
 										<Col>{product.countInStock > 0 ? 'In stock' : 'Out of stock'}</Col>
 									</Row>
 								</ListGroup.Item>
+
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={qty}
+													onChange={(e) => setQty(e.target.value)}
+												>
+													{/* When Array is called as a function rather than as a constructor, it creates and initialises a new Array object. Thus the function call Array(...) is equivalent to the object creation expression new Array(...) with the same arguments. */}
+													{/* so...create an array of length = countInStock.  spread the keys (so the indices starting at 0) of this new array into another array.  this way we obtain the numbers to display in the qty options, ranging from 1 to the countInStock (once we add 1 to x)  */}
+													{/* [0,1,2,3,4] */}
+													{[...Array(product.countInStock).keys()].map(x => (
+													<option key={x + 1} value={x + 1}>
+														{x + 1}
+													</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
+
 								<ListGroup.Item>
 									<Button
 										className='btn-block'
 										type='button'
-										disabled={product.countInStock === 0}
+                    disabled={product.countInStock === 0}
+                    onClick={addToCartHandler}
 									>
 										Add To Cart
 									</Button>
