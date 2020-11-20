b78f77658da51bfb137f4da5b66128e5040e43d7
xtophe xanon <xocon10@gmail.com>
2020-11-19 20:14:56 -0600

    create auth middleware and add to get user profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,27 +6,47 @@ import User from '../models/userModel.js'
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
+	const { email, password } = req.body
 
-  const user = await User.findOne({ email })
+	const user = await User.findOne({ email })
 
-  // call instance method to match pw
-  if(user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      // call function to create and sign token, passing the user's id which will be embedded in the token and we can access when we decode
-      token: generateToken(user._id)
-    })
-  } else {
-    // 404 - Unauthorized
-    res.status(404)
-    throw new Error('Invalid email or passwod')
-  }
+	// call instance method to match pw
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id     : user._id,
+			name    : user.name,
+			email   : user.email,
+			isAdmin : user.isAdmin,
+			// call function to create and sign token, passing the user's id which will be embedded in the token and we can access when we decode
+			token   : generateToken(user._id)
+		})
+	} else {
+		// 404 - Unauthorized
+		res.status(404)
+		throw new Error('Invalid email or passwod')
+	}
 })
 
-export { 
-  authUser
-}
+// @desc    GET user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+	// if req passed the 'protect' middleware on the route, the user's id will be available on req.user
+	const user = await User.findById(req.user._id)
+
+	if (user) {
+		res.json({
+			_id     : user._id,
+			name    : user.name,
+			email   : user.email,
+			isAdmin : user.isAdmin
+		})
+	} else {
+		res.status(404)
+		throw new Error('User not found')
+	}
+
+	res.send('Success')
+})
+
+export { authUser, getUserProfile }
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+const protect = asyncHandler(async (req, res, next) => {
+	let token
+	// console.log(req.headers.authorization)
+
+	// https://www.w3schools.com/jsref/jsref_startswith.asp
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		// console.log('token found')
+		try {
+			// token will be after the space following 'Bearer ' so use .split()[1]
+			token = req.headers.authorization.split(' ')[1]
+			const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+			// user's id will be in decoded.id.  we can assign a new property on req (req.user) and store the user that comes back from the db (excluding the password).  we will now have access to the req.user in all of the protected routes
+			req.user = await User.findById(decoded.id).select('-password')
+
+			next()
+		} catch (error) {
+			console.error(error)
+			res.status(401)
+			throw new Error('Not authorized, token failed')
+		}
+	}
+
+	if (!token) {
+		res.status(404)
+		throw new Error('Not authorized, no token')
+	}
+})
+
+export { protect }
