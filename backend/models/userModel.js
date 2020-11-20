ae2cdf217aebfd214cce5f9cc34aa1596fd4d560
xtophe xanon <xocon10@gmail.com>
2020-11-19 21:05:00 -0600

    create register route.  create middleware to hash user password on register

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,10 +28,24 @@ const userSchema = mongoose.Schema(
 	}
 )
 
-// create a method we can access with an instantiated User (this)
+// create a method we can access with an instantiated User (this) to match plaintext password from login form to hashed pw in db 
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+// middleware to hash password when user signs up/registers
+userSchema.pre('save', async function(next) {
+  // only run hash middleware if there is a modified (or newly created) password so if user updates name or email, it doesn't generate new pw hash which would make their old pw not work
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  // create a salt
+  const salt = await bcrypt.genSalt(10)
+
+  // update password of User instance to be a hashed version of that password
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
