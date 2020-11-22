857dd18b260ec086714c0c432d03a2ca3ea16948
xtophe xanon <xocon10@gmail.com>
2020-11-22 06:53:00 -0600

    add user details to navbar.  implement logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from '../constants/userConstants'
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
 	try {
@@ -20,7 +20,8 @@ export const login = (email, password) => async (dispatch) => {
 			payload : data
 		})
 
-		localStorage.setItem('userInfo', JSON.stringify(data))
+    localStorage.setItem('userInfo', JSON.stringify(data))
+    
 	} catch (error) {
 		dispatch({
 			type    : USER_LOGIN_FAIL,
@@ -30,4 +31,9 @@ export const login = (email, password) => async (dispatch) => {
 					: error.message
 		})
 	}
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT})
 }
