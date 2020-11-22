d7a6caaafc2e60054803e73f9df82b0bf21d1468
xtophe xanon <xocon10@gmail.com>
2020-11-22 16:42:40 -0600

    create and populate PlaceOrderScreen from global state.  calculate payment.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from './screens/LoginScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -23,6 +24,7 @@ const App = () => {
 					<Route path='/profile' component={ProfileScreen} />
 					<Route path='/shipping' component={ShippingScreen} />
 					<Route path='/payment' component={PaymentScreen} />
+					<Route path='/placeorder' component={PlaceOrderScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
 					{/* optional id */}
 					<Route path='/cart/:id?' component={CartScreen} />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,10 +18,12 @@ const reducer = combineReducers({
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+// optional to load payment method from LS...if so, add paymentMethod: paymentMethodFromStorage to initialState below
+// const paymentMethodFromStorage = localStorage.getItem('paymentMethod') ? JSON.parse(localStorage.getItem('paymentMethod')) : null
 
 const initialState = {
   cart: {cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
-  userLogin: { userInfo: userInfoFromStorage }
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [ thunk ]
