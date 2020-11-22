4ee7726056d303768436e4e89792b6aba47d6dee
xtophe xanon <xocon10@gmail.com>
2020-11-22 09:33:25 -0600

    add route and controller to update user profile in db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,30 +10,29 @@ const registerUser = asyncHandler(async (req, res) => {
 
 	const userExists = await User.findOne({ email })
 
-  if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
-  }
+	if (userExists) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
 
-  const user = await User.create({
-    name,
-    email,
-    password
-  })
+	const user = await User.create({
+		name,
+		email,
+		password
+	})
 
-  if (user) {
-    res.status(201).json({
-      _id     : user._id,
+	if (user) {
+		res.status(201).json({
+			_id     : user._id,
 			name    : user.name,
 			email   : user.email,
 			isAdmin : user.isAdmin,
 			token   : generateToken(user._id)
-    })
-  } else {
-    res.status(400)
-    throw new Error('Invalid user data')
-  }
-
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid user data')
+	}
 })
 
 // @desc    Auth user & get token
@@ -61,14 +60,11 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 })
 
-
-
-
 // @desc    GET user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-	// if req passed the 'protect' middleware on the route, the user's id will be available on req.user
+	// if req passed the 'protect' middleware on the route, the user's id (from the token) will be available on req.user
 	const user = await User.findById(req.user._id)
 
 	if (user) {
@@ -82,7 +78,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
 		res.status(404)
 		throw new Error('User not found')
 	}
-
 })
 
-export { authUser, getUserProfile, registerUser }
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id)
+
+	if (user) {
+		user.name = req.body.name || user.name
+		user.email = req.body.email || user.email
+		if (req.body.password) {
+			user.password = req.body.password
+		}
+
+		const updatedUser = await user.save()
+		res.json({
+			_id     : updatedUser._id,
+			name    : updatedUser.name,
+			email   : updatedUser.email,
+			isAdmin : updatedUser.isAdmin,
+			token   : generateToken(updatedUser._id)
+		})
+	} else {
+		res.status(404)
+		throw new Error('User not found')
+	}
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
