e969594753c369545395e3e2105193c1969b33d0
xtophe xanon <xocon10@gmail.com>
2020-11-23 16:12:36 -0600

    add constants, action and reducer to get order by id

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -95,8 +95,8 @@ const PlaceOrderScreen = ({ history }) => {
 													<Link to={`/product/${item.product}`}>{item.name}</Link>
 												</Col>
 												<Col md={4}>
-													{/* {item.qty} x ${item.price} = ${(item.qty * item.price).toFixed(2)} */}
-													{item.qty} x ${item.price} = ${item.qty * item.price}
+													{item.qty} x ${item.price} = ${(item.qty * item.price).toFixed(2)}
+													{/* {item.qty} x ${item.price} = ${item.qty * item.price} */}
 												</Col>
 											</Row>
 										</ListGroup.Item>
