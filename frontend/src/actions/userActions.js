afc18580e0063ce80fbd69a1d0045dab4a79f146
xtophe xanon <xocon10@gmail.com>
2020-11-28 22:05:12 -0600

    create constants, action and reducer for admin access to user list.  create UserListScreen to display data.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,12 +8,15 @@ import {
 	USER_REGISTER_SUCCESS,
 	USER_REGISTER_FAIL,
 	USER_DETAILS_REQUEST,
-  USER_DETAILS_SUCCESS,
-  USER_DETAILS_FAIL,
-  USER_DETAILS_RESET,
-  USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL
+	USER_DETAILS_SUCCESS,
+	USER_DETAILS_FAIL,
+	USER_DETAILS_RESET,
+	USER_UPDATE_PROFILE_REQUEST,
+	USER_UPDATE_PROFILE_SUCCESS,
+	USER_UPDATE_PROFILE_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -29,7 +32,7 @@ export const register = (name, email, password) => async (dispatch) => {
 			}
 		}
 
-    // add user to db
+		// add user to db
 		const { data } = await axios.post('/api/users', { name, email, password }, config)
 
 		dispatch({
@@ -43,7 +46,6 @@ export const register = (name, email, password) => async (dispatch) => {
 			payload : data
 		})
 
-    
 		localStorage.setItem('userInfo', JSON.stringify(data))
 	} catch (error) {
 		dispatch({
@@ -90,7 +92,7 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
 	localStorage.removeItem('userInfo')
 	dispatch({ type: USER_LOGOUT })
-	dispatch({ type: USER_DETAILS_RESET})
+	dispatch({ type: USER_DETAILS_RESET })
 	dispatch({ type: ORDER_LIST_MY_RESET })
 }
 
@@ -112,7 +114,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
 			}
 		}
 
-    // /api/users/profile -- why passing in id here?  perhaps future endpoints will need it
+		// /api/users/profile -- why passing in id here?  perhaps future endpoints will need it
 		const { data } = await axios.get(`/api/users/${id}`, config)
 
 		dispatch({
@@ -151,18 +153,42 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
 		dispatch({
 			type    : USER_UPDATE_PROFILE_SUCCESS,
 			payload : data
-    })
-
-		dispatch({
-			type    : USER_LOGIN_SUCCESS,
-			payload : data
-    })
-
-    localStorage.setItem('userInfo', JSON.stringify(data))
-    
+		})
 	} catch (error) {
 		dispatch({
 			type    : USER_UPDATE_PROFILE_FAIL,
+			payload :
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message
+		})
+	}
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type : USER_LIST_REQUEST
+		})
+
+		// destructure nested userInfo object
+		const { userLogin: { userInfo } } = getState()
+
+		const config = {
+			headers : {
+				Authorization : `Bearer ${userInfo.token}`
+			}
+		}
+
+		const { data } = await axios.get(`/api/users`, config)
+
+		dispatch({
+			type    : USER_LIST_SUCCESS,
+			payload : data
+		})
+	} catch (error) {
+		dispatch({
+			type    : USER_LIST_FAIL,
 			payload :
 				error.response && error.response.data.message
 					? error.response.data.message
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,13 +5,13 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 
 const Header = () => {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
 	const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
-  
-  const logoutHandler = () =>  {
-    dispatch(logout())
-  }
+	const { userInfo } = userLogin
+
+	const logoutHandler = () => {
+		dispatch(logout())
+	}
 
 	return (
 		<header>
@@ -29,20 +29,34 @@ const Header = () => {
 									<i className='fas fa-shopping-cart' />Cart
 								</Nav.Link>
 							</LinkContainer>
-              {/* if user is logged in ie their userInfo is in global state via previous action or LS */}
+							{/* if user is logged in ie their userInfo is in global state via previous action or LS */}
 							{userInfo ? (
 								<NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
-                </NavDropdown>
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+								</NavDropdown>
 							) : (
 								<LinkContainer to='/login'>
 									<Nav.Link>
 										<i className='fas fa-user' />Sign In
 									</Nav.Link>
 								</LinkContainer>
+							)}
+							{userInfo &&
+							userInfo.isAdmin && (
+								<NavDropdown title='Admin' id='adminmenu'>
+									<LinkContainer to='admin/userlist'>
+										<NavDropdown.Item>Users</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to='/admin/productlist'>
+										<NavDropdown.Item>Products</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to='/admin/orderlist'>
+										<NavDropdown.Item>Orders</NavDropdown.Item>
+									</LinkContainer>
+								</NavDropdown>
 							)}
 						</Nav>
 					</Navbar.Collapse>
