a8042ee94a7de7315afc69b3fd33d8c3d691b712
xtophe xanon <xocon10@gmail.com>
2020-11-28 21:39:02 -0600

    add route, controller to get all users and add  middleware to restrict route to admin only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,19 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import {
+	authUser,
+	registerUser,
+	getUserProfile,
+	updateUserProfile,
+	getUsers
+} from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser)
+
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 // add middleWare as second arg
 router.post('/login', authUser)
 // to implement middleware, put the function as a first arg to route.get()
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
- 
