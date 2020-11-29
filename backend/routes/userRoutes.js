e3f93696d9c58d16e502f88d88acff726d949cdd
xtophe xanon <xocon10@gmail.com>
2020-11-29 11:29:10 -0600

    create route and controller to allow admin user to get user by id and update user by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,17 +5,22 @@ import {
 	registerUser,
 	getUserProfile,
 	updateUserProfile,
-  getUsers,
-  deleteUser
+	getUsers,
+	deleteUser,
+	getUserById,
+	updateUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
-
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 // add middleWare as second arg
 router.post('/login', authUser)
 // to implement middleware, put the function as a first arg to route.get()
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+	.route('/:id')
+	.delete(protect, admin, deleteUser)
+	.get(protect, admin, getUserById)
+	.put(protect, admin, updateUser)
 
 export default router
