741383f8c9dd6aaa797bcdec23f4b234220117aa
xtophe xanon <xocon10@gmail.com>
2020-11-30 07:20:22 -0600

    configure express to handle file uploads

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
 // to use ES modules in Node (v14.6+), add to package.json...also note when using import syntax in Node, you must add the file type extension
 // "type": "module",
 
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -14,6 +15,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -27,9 +29,14 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 // route to provide safely stored (in .env) paypal id to client
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+// need to mimic default path __dirname behavior since it is not available with es modules syntax
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/', (req, res) => {
   res.send('API is running')
